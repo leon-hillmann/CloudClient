d0d8f84c11c529c76037b9ac10d2aca09f649430
Leon <Leon@Leon-PC>
2015-11-01 18:37:59 +0100

    3. Clean imports

--- CloudClient/src/application/Main.java
+++ CloudClient/src/application/Main.java
@@ -1,6 +1,4 @@
 package application;
-	
-import java.io.IOException;
 
 import application.net.CloudFile;
 import application.net.ConnectionSocket;
@@ -8,8 +6,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
@@ -61,7 +57,4 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
-	
 }
