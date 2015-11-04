94838a3d775e7dbfed8060c976da83e7f02079f3
Leon <Leon@Leon-PC>
2015-11-04 18:25:24 +0100

    5. Experimental encryption

--- CloudClient/src/application/Main.java
+++ CloudClient/src/application/Main.java
@@ -1,7 +1,18 @@
 package application;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 import application.net.CloudFile;
 import application.net.ConnectionSocket;
@@ -16,16 +27,14 @@ public class Main extends Application {
 	ConnectionSocket connection;
 	BrowserController browser;
 	CloudFile rootDirectory;
-	
-	PublicKey serv_pub;
+	SecretKey aes_key;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			connection = new ConnectionSocket("pizzakatze.no-ip.org", 1337);
 			
-			serv_pub = getServerKey();
-			System.out.println(serv_pub.toString());
+			keyExchange();
 			
 			rootDirectory = getServerRoot();
 			if(rootDirectory == null){
@@ -46,6 +55,36 @@ public class Main extends Application {
 			e.printStackTrace();
 			System.exit(0);
 		}
+	}
+	
+	private void keyExchange() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException{
+		System.out.println("Key exchange");
+		System.out.println("Generating AES key");
+		KeyGenerator aesgen = KeyGenerator.getInstance("AES");
+		SecretKey aes_key = aesgen.generateKey();
+		
+		System.out.println("Requesting server key");
+		PublicKey spub = getServerKey();
+		connection.waitUntilOK();
+		System.out.println("Encrypting connection");
+		Cipher c = Cipher.getInstance("RSA");
+		c.init(Cipher.ENCRYPT_MODE, spub);
+		connection.setOutputCipher(c);
+		System.out.println("Sending key");
+		connection.sendCommand(Command.AES_TRANSMISSION);
+		connection.sendObject(aes_key);
+		
+		connection.waitUntilOK();
+		
+		System.out.println("Encrypting the connection (AES)");
+		Cipher cin = Cipher.getInstance("AES");
+		Cipher cout = Cipher.getInstance("AES");
+		cin.init(Cipher.DECRYPT_MODE, aes_key);
+		cout.init(Cipher.ENCRYPT_MODE, aes_key);
+		
+		connection.setOutputCipher(cout);
+		connection.setInputCipher(cin);
+		this.aes_key = aes_key;
 	}
 	
 	private PublicKey getServerKey(){
--- CloudClient/src/application/net/ConnectionSocket.java
+++ CloudClient/src/application/net/ConnectionSocket.java
@@ -3,37 +3,97 @@ package application.net;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 import application.Command;
 
 public class ConnectionSocket {
-	
+
 	private Socket socket;
+	private InputStream is;
+	private OutputStream os;
 	private DataInputStream dis;
 	private DataOutputStream dos;
 	private ObjectInputStream ois;
-	
-	public ConnectionSocket(String ipAddress, int port) throws UnknownHostException, IOException{
+	private ObjectOutputStream oos;
+	private String ipAdd;
+	private int port;
+
+	public ConnectionSocket(String ipAddress, int port) throws UnknownHostException, IOException, NoSuchAlgorithmException{
+		this.ipAdd = ipAddress;
+		this.port = port;
 		socket = new Socket(ipAddress, port);
-		dos = new DataOutputStream(socket.getOutputStream());
-		dis = new DataInputStream(socket.getInputStream());
-		ois = new ObjectInputStream(dis);
+		dos = new DataOutputStream(os = socket.getOutputStream());
+		dis = new DataInputStream(is = socket.getInputStream());
+		ois = new ObjectInputStream(is);
+		oos = new ObjectOutputStream(os);
 	}
-	
+
 	private Command readCommand() throws IOException{
 		Command cmd = Command.get(dis.readInt());
 		System.out.println("SERVER:> " + cmd);
 		return cmd;
 	}
-	
+
 	public void sendCommand(Command cmd) throws IOException{
 		dos.writeInt(cmd.getCode());
 		dos.flush();
 	}
-	
+
+	public void setOutputCipher(Cipher c){
+		try {
+			dos = null;
+			oos = null;
+			System.out.println("Create CipherOutputStream");
+			CipherOutputStream cos = new CipherOutputStream(os = socket.getOutputStream(), c);
+			System.out.println("Create DataOutputStream");
+			dos = new DataOutputStream(cos);
+			System.out.println("Create ObjectOutputStream");
+			oos = new ObjectOutputStream(dos);
+			System.out.println("----------Done!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void setInputCipher(Cipher c){
+		try {
+			dis = null;
+			ois = null;
+			System.out.println("Create CipherInputStream");
+			CipherInputStream cis = new CipherInputStream(is = socket.getInputStream(), c);
+			System.out.println("Create DataInputStream");
+			dis = new DataInputStream(cis);
+			System.out.println("----------Done!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void sendObject(Object o){
+		try{
+			sendCommand(Command.OBJECT_TRANSMISSION);
+			oos.writeObject(o);
+			oos.flush();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
 	public Object sendObjectrCommand(Command cmd) throws IOException, ClassNotFoundException{
 		dos.writeInt(cmd.getCode());
 		dos.flush();
@@ -48,17 +108,31 @@ public class ConnectionSocket {
 		}
 		return null;
 	}
-	
+
+	public void waitUntilOK(){
+		System.out.println("Waiting for server ok");
+		try{
+			Command c;
+			while((c = Command.get(dis.readInt())) != Command.OK){
+				System.out.println(c);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
 	public void close(){
 		try{
 			sendCommand(Command.CLOSE);
 			dis.close();
 			dos.close();
+			oos.close();
+			ois.close();
 			socket.close();
 			System.out.println("Connection closed");
 		}catch(IOException e){
-			
+
 		}
 	}
-	
+
 }
